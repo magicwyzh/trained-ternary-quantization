b2a45cf74e2bed255e281593811c1415ce11418d
TropComplique <antoshchenko.b@gmail.com>
2017-08-22 10:17:21 +0300

    small fix

--- vanilla_densenet/get_densenet.py
+++ vanilla_densenet/get_densenet.py
@@ -26,7 +26,7 @@ def get_model():
         p for n, p in model.named_parameters()
         if 'norm' in n and 'bias' in n
     ]
-    
+
     # parameter initialization
     for p in weights:
         kaiming_uniform(p)
